76bad77b2ae96bebc7d90adece6600519094a63b
zhxyu <zxyuayaka@gmail.com>
2023-10-15 21:37:21 +0800

    10.15

--- assignment1/neff_plot.m
+++ assignment1/neff_plot.m
@@ -6,10 +6,11 @@ lambda = 1.48;   % wavelength (um)
 
 % Layer heights:
 h1 = 2.0;           % Lower cladding
-h2 = 2.7280;           % Core thickness
+h2 = 2.723;           % Core thickness
 h3 = 2.0;           % Upper cladding
 
 % the fundamental TE mode:
-neff = planar_waveguide_TE(n1, n2, lambda, h1, h2, h3);
+%neff = planar_waveguide_TE(n1, n2, lambda, h1, h2, h3);
+[nTE, nTM] = wg_1D_analytic (lambda, h2, n2, n1, n2);
 fprintf(1,'neff = %.4f\n',neff);
 
